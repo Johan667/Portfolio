7ddc77bb669f2dd2037e36070d63c54c7cde739a
Johan667 <johan.kasri@icloud.com>
2021-10-08 14:13:37 +0200

    init

--- login.php
+++ login.php
@@ -1,96 +1,71 @@
 <?php
-// http://localhost:8000
 
 require_once 'config/framework.php';
 require_once 'config/connect.php';
-
-$error=null;
-
-
-if (isset($_POST['token']) && $_POST['token'] === $_SESSION['token']) {
-  if(isset($_POST['pseudo']) && !empty($_POST['pseudo'])){
-    echo "<br>Veuillez remplir le champ";
-  }
-  if (strlen($_POST['pseudo']) < 4 ) {
-    echo '<br>Pseudo trop petit';
-  }
-  if (30< strlen($_POST['pseudo'])) {
-    echo '<br>Pseudo trop grand';
-  }
-  if(isset($_POST['email']) && !empty($_POST['email'])){
-      echo "<br>Veuillez remplir le champ";
-  }
-  if(isset($_POST['password']) && !empty($_POST['password'])){
-    echo'<br>Mot de passe incorrecte';
-  }
-  if($_POST['password'] === $_POST['passwordConf'] ){
-    echo'<br>Ce ne sont pas les même mots de passe';
-  }
-}
+// session_start(); //
+$errors = [];
 
 
-
-
-
-
-
+        if (isset($_POST['pseudo'])){
+          $username = stripslashes($_REQUEST['pseudo']);
+          $username = mysqli_real_escape_string($conn, $pseudo);
+          $password = stripslashes($_REQUEST['password1']);
+          $password = mysqli_real_escape_string($conn, $password1);
+            $query = "SELECT * FROM `users` WHERE username='$pseudo' and password='".password_verify('token', $password1)."'";
+          $result = mysqli_query($conn,$query) or die(mysql_error());
+          $rows = mysqli_num_rows($result);
+          if($rows==1){
+              $_SESSION['pseudo'] = $pseudo;
+              header("Location: index.php");
+          }else{
+            $message = "Le nom d'utilisateur ou le mot de passe est incorrect.";
+          }
+        }
+ 
 
 ?>
 
-
-
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="fr">
   <head>
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-    />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
-    <title>S'identifier</title>
+    <title>Johan Kasri</title>
   </head>
+ 
+
   <body>
     <div class="m-a">
       <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1 m-auto">
         <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
-          Identifiez Vous 
+         <em> Connectez Vous</em>
         </p>
 
         <form class="mx-1 mx-md-4" method="post" >
           <input type="hidden"name="token" value="<?= miniToken(); ?>">
-          <div class="d-flex flex-row align-items-center mb-4">
-            <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-            <div class="form-outline flex-fill mb-0">
-              <input type="text"  class="form-control" name="pseudo" id="pseudo"/>
-              <label class="form-label" for="pseudo">Pseudo</label>
-            </div>
-          </div>
 
           <div class="d-flex flex-row align-items-center mb-4">
             <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
-            <div class="form-outline flex-fill mb-0">
+            <label class="form-label" for="email">E-mail</label>
+            <div class="form-outline flex-fill mb-0">          
               <input type="email"  class="form-control" name="email" id="email"/>
-              <label class="form-label" for="email">E-mail</label>
+              
             </div>
           </div>
 
           <div class="d-flex flex-row align-items-center mb-4">
             <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+            <label class="form-label" for="password">Mot de passe</label>
             <div class="form-outline flex-fill mb-0">
-              <input type="password"  class="form-control"  name="password" id="password"/>
-              <label class="form-label" for="password">Mot de passe</label>
+              <input type="password"  class="form-control"  name="password1" id="password1"/>
+             
             </div>
           </div>
 
-          
-          </div>
+
 
           <div class="d-flex justify-content-center m-4">
             <button type="submit" class="btn btn-primary ">S'inscrire</button>
@@ -103,4 +78,18 @@ if (isset($_POST['token']) && $_POST['token'] === $_SESSION['token']) {
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
   </body>
+</html>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    -->
+
+  </body>
+
 </html>
