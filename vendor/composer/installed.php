843677abdf5a4d049708de751523caf03b6e0e80
Johan667 <johan.kasri@icloud.com>
2021-10-11 15:41:15 +0200

    lundiamm

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f21cf8d33281bbf32f70cc39372a536f5caa661a',
+        'name' => 'stagiaire/portfolio',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fzaninotto/faker' => array(
+            'pretty_version' => 'v1.9.2',
+            'version' => '1.9.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../fzaninotto/faker',
+            'aliases' => array(),
+            'reference' => '848d8125239d7dbf8ab25cb7f054f1a630e68c2e',
+            'dev_requirement' => true,
+        ),
+        'stagiaire/portfolio' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f21cf8d33281bbf32f70cc39372a536f5caa661a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
