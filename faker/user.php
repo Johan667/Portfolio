68e1e87d2e84ff88a860c196d59bdba7fd59a357
Johan667 <johan.kasri@icloud.com>
2021-10-11 15:18:38 +0200

    apremmm

--- faker/user.php
+++ faker/user.php
@@ -7,9 +7,13 @@ require_once '../config/framework.php';
 require_once '../config/connect.php';
 
 $faker = Factory::create(); 
-
+$faker = Faker\Factory::create('fr_FR');
 for ($i = 0; $i < 10; $i++){
-    echo $faker->name. "<br>";
-    echo $faker->freeEmailDomain. "<br>";
+   // echo $faker->name. "<br><br>";
+   // echo $faker->freeEmailDomain. "<br><br>";//
+   // echo $faker->name.'@'.$faker->freeEmailDomain. "<br><br>"; //
+    echo $faker->name.':<br> '.$faker->email. "<br><br>";
     
 }
+
+?>
